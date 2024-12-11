71681c21d31450156ef4e83321c9ef61550cedc5
minhkhue <jeanphan19851@yahoo.fr>
2024-12-11 14:18:41 +0700

    Update main.py

--- main.py
+++ main.py
@@ -480,6 +480,42 @@ if selected == 'Thông tin về sản phẩm':
 
                     add_five_cmt_button = st.button("More comments", on_click=add_5_comment, key="add_five_cmt_button")
 
+                    with elements("dashboard"):
+
+                        # You can create a draggable and resizable dashboard using
+                        # any element available in Streamlit Elements.
+                    
+                    
+                        # First, build a default layout for every element you want to include in your dashboard
+                    
+                        layout = [
+                            # Parameters: element_identifier, x_pos, y_pos, width, height, [item properties...]
+                            dashboard.Item("first_item", 0, 0, 2, 2),
+                            dashboard.Item("second_item", 2, 0, 2, 2, isDraggable=False, moved=False),
+                            dashboard.Item("third_item", 0, 2, 1, 1, isResizable=False),
+                        ]
+                    
+                        # Next, create a dashboard layout using the 'with' syntax. It takes the layout
+                        # as first parameter, plus additional properties you can find in the GitHub links below.
+                    
+                        with dashboard.Grid(layout):
+                            mui.Paper("First item", key="first_item")
+                            mui.Paper("Second item (cannot drag)", key="second_item")
+                            mui.Paper("Third item (cannot resize)", key="third_item")
+                    
+                        # If you want to retrieve updated layout values as the user move or resize dashboard items,
+                        # you can pass a callback to the onLayoutChange event parameter.
+                    
+                        def handle_layout_change(updated_layout):
+                            # You can save the layout in a file, or do anything you want with it.
+                            # You can pass it back to dashboard.Grid() if you want to restore a saved layout.
+                            print(updated_layout)
+                    
+                        with dashboard.Grid(layout, onLayoutChange=handle_layout_change):
+                            mui.Paper("First item", key="first_item")
+                            mui.Paper("Second item (cannot drag)", key="second_item")
+                            mui.Paper("Third item (cannot resize)", key="third_item")
+
                 with info_tabs[2]:
                     filtered_product = selected_product.groupby('ma_san_pham')['processed_noi_dung_binh_luan'].apply(' '.join).reset_index()
                     filtered_product.rename(columns={"processed_noi_dung_binh_luan": "merged_comments"}, inplace=True)
