8a61ce235da9061f8358ef2c305da74b4a758eec
minhkhue <jeanphan19851@yahoo.fr>
2024-12-11 13:14:45 +0700

    Update main.py

--- main.py
+++ main.py
@@ -474,7 +474,7 @@ if selected == 'Thông tin về sản phẩm':
                             st.write(f'{filtered_reviews["ngay_binh_luan"].dt.strftime("%d-%m-%Y").values[index]}, {filtered_reviews["ho_ten"].values[index]}, {filtered_reviews["so_sao"].values[index] * ":star:"}')
                             st.write(f'{filtered_reviews["noi_dung_binh_luan"].values[index]}')
                             st.write('-' * 3)
-                    slide_val = st.slider("Pick a number", 0, filtered_reviews_count,value = 5, key="slider_danhgia",label_visibility="collapsed")
+                    slide_val = st.slider("Pick a number", 0, filtered_reviews_count,step=5,value = 5, key="slider_danhgia",label_visibility="collapsed")
                     add_five_cmt_button = st.button("More comments", on_click=add_5_comment, key="add_five_cmt_button")
 
                     for i in range(slide_val):
